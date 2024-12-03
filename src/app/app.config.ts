9d9b8333aa09edd7a75bd3ba326a18f09acc779f
Kodadala Praveen Kumar <praveenk@vector97.com>
2024-12-04 04:20:33 +0530

    app.config is modified

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideNzIcons } from './icons-provider';
+import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
 
 import { routes } from './app.routes';
 
+registerLocaleData(en);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideNzIcons(), provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withJsonpSupport()),
+    provideAnimationsAsync(),
+    provideNzIcons(icons),
+    provideNzI18n(en_US),
+    provideRouter(routes)
+  ]
 };
