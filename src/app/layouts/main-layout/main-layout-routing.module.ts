6c65fc4423f28851ab573c10fd302fead1a81922
praveengetida <praveen@getida.com>
2025-02-12 11:52:38 -0500

    basic setup is done in home comp

--- src/app/layouts/main-layout/main-layout-routing.module.ts
+++ src/app/layouts/main-layout/main-layout-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FeedComponent } from '../../features/feed/feed.component';
 import { HomeComponent } from '../../home/home.component';
 
 export const routes: Routes = [
-  { path: 'feed', component: FeedComponent },
   { path: 'home', component: HomeComponent },
 ];
 
